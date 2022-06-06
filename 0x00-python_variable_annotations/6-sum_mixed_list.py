23453243fbec0d8e0ea766a78198278c98c41013
juanescorreap <juanestebancope@outlook.com>
2022-06-06 15:57:25 -0500

    change the order of the imports

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 type-annotated function sum_mixed_list which takes a list mxd_lst
 of integers and floats and returns their sum as a float
 """
-from typing import Union, List
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
