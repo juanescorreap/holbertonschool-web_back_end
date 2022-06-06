7a6e3b1dbb54bdd8a4e5886b38c1ccf265e14e2d
juanescorreap <juanestebancope@outlook.com>
2022-06-06 17:26:02 -0500

    test to find missing requirement

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
-The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float.
+type-annotated function to_kv that takes a string k and an int OR float
+v as arguments and returns a tuple.
 """
-from typing import Union, Tuple
+from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Funtion that takes a strin and an int or float and returns a tuple"""
-    new_tuple: tuple = (k, v*v)
-    return new_tuple
+    """ returns a tuple"""
+    my_tuple = (k, v * v)
+    return my_tuple
