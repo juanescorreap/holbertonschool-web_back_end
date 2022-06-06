d595993aae3ab95afcb537472ea363932dc60ff1
juanescorreap <juanestebancope@outlook.com>
2022-06-06 17:27:02 -0500

    change indentation to tabs

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-type-annotated function to_kv that takes a string k and an int OR float
-v as arguments and returns a tuple.
+type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.
+The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float.
 """
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ returns a tuple"""
-    my_tuple = (k, v * v)
-    return my_tuple
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, fl]:
+    """Funtion that takes a strin and an int or float and returns a tuple"""
+    new_tuple: tuple = (k, v*v)
+    return new_tuple
