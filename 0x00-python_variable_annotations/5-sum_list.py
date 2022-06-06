3bb10a3f2f22a563c55a4db77e33a2c40f0146d3
juanescorreap <juanestebancope@outlook.com>
2022-06-06 15:48:05 -0500

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type-annotated function sum_list which takes a list input_list
+of floats as argument and returns their sum as a float.
+"""
+from ast import List
+
+
+def sum_list(input_list: List[float])-> float:
+    """Function that takes a list of floats and returns their sum"""
+    return sum(input_list)
