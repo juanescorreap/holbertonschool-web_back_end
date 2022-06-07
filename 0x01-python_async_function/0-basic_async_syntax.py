1fb48480c72412158bad45aaef6ec35a78449855
juanescorreap <juanestebancope@outlook.com>
2022-06-07 14:59:50 -0500

    added a range for the random number to be generated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,6 +10,6 @@ import random
 
 
 async def wait_random(max_delay=10):
-    random_number = random()
+    random_number = random.uniform(0.0, max_delay)
     await asyncio.sleep(random_number)
     return random_number
