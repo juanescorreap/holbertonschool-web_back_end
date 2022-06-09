c2e4c85dca5ddc0685f1d001e08dd0d32f5103b6
juanescorreap <juanestebancope@outlook.com>
2022-06-09 17:05:03 -0500

    task 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Class LIFOCache that inherits from
+BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Class LIFOCache that inherits from
+    BaseCaching and is a caching system
+    """
+    def __init__(self):
+        super().__init__()
+        self.record_of_keys = []
+
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            key_to_delte = self.record_of_keys[len(self.record_of_keys) - 1]
+            del self.cache_data[key_to_delte]
+            print("DISCARD: {}".format(key_to_delte))
+        self.record_of_keys.append(key)
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
