421988472bacbd04d0f1432af79026a9599559ed
juanescorreap <juanestebancope@outlook.com>
2022-06-09 12:07:28 -0500

    task 0

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Class BasicCache that inherits from
+BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Class BasicCache that inherits from
+    BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        if key or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
