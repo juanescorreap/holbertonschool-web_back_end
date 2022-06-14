175941eb4ea32c3ac62b4b2bbb7a7afd258b7997
juanescorreap <juanestebancope@outlook.com>
2022-06-14 11:57:19 -0500

    fix indentation for method get_hyper

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -49,20 +49,20 @@ class Server:
         return self.dataset()[upper_limit:lower_limit]
 
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """
-    get_hyper method that takes the same
-    arguments (and defaults) as get_page and returns
-    a dictionary containing the following key-value pairs
-    """
-    assert type(page) is int and page > 0
-    assert type(page_size) is int and page_size > 0
-    total_pages = math.floor(len(self.dataset()) / page_size)
-    data_dict = {'page_size': len(self.get_page(page, page_size)),
-                 'page': page,
-                 'data': self.get_page(page, page_size),
-                 'next_page': page + 1 if page + 1 < total_pages else None,
-                 'prev_page': page - 1 if page > 1 else None,
-                 'total_pages': total_pages
-                 }
-    return data_dict
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get_hyper method that takes the same
+        arguments (and defaults) as get_page and returns
+        a dictionary containing the following key-value pairs
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        total_pages = math.floor(len(self.dataset()) / page_size)
+        data_dict = {'page_size': len(self.get_page(page, page_size)),
+                    'page': page,
+                    'data': self.get_page(page, page_size),
+                    'next_page': page + 1 if page + 1 < total_pages else None,
+                    'prev_page': page - 1 if page > 1 else None,
+                    'total_pages': total_pages
+                    }
+        return data_dict
