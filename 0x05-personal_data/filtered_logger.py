8c96ed221a0ca2a3299a7532281850fd4c2b0427
juanescorreap <juanestebancope@outlook.com>
2022-06-15 14:43:59 -0500

    task 0

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Function called filter_datum that returns the log
+message obfuscated
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Function called filter_datum that returns the log
+    message obfuscated
+    """
+    for field in fields:
+        obfuscated = re.sub(fr'{field}=.+?{separator}', f'{field}={redaction}{separator}',
+                            message)
+    return obfuscated
