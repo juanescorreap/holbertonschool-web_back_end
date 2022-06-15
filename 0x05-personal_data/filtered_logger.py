273769d1cb2c6990fdce5764f43e44403134f76c
juanescorreap <juanestebancope@outlook.com>
2022-06-15 17:44:29 -0500

    use the message parameter to return the results so that each field is correctly obfuscated

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 """
-Function called filter_datum that returns the log
-message obfuscated
+
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -14,6 +31,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     message obfuscated
     """
     for field in fields:
-        obfuscated = re.sub(fr'{field}=.+?{separator}', f'{field}={redaction}{separator}',
-                            message)
-    return obfuscated
+        message = re.sub(fr'{field}=.+?{separator}', f'{field}={redaction}{separator}',
+                         message)
+    return message
