0214c6bbf674f1ff8572e1535fa914633931b3c5
juanescorreap <juanestebancope@outlook.com>
2022-06-16 12:50:17 -0500

    task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -13,3 +13,10 @@ def hash_password(password: str) -> bytes:
     which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Function that validates that the provided
+    password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
