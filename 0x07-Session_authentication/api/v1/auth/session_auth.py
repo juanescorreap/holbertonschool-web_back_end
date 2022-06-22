999520cd5a1d3b6ff4bfff6db42da9505a6426b5
juanescorreap <juanestebancope@outlook.com>
2022-06-22 16:21:41 -0500

    method to create a session id for an user_id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,23 @@
 Class to manage an API authentication mechanism
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Class to manage an API authentication mechanism
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return user_id
