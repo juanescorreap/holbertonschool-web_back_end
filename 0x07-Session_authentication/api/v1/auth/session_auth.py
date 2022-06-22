821ef80678a03de6a9b0b6953b92da602e4aef90
juanescorreap <juanestebancope@outlook.com>
2022-06-22 16:00:46 -0500

    add new Class to manage an API authentication mechanism

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Class to manage an API authentication mechanism
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class to manage an API authentication mechanism
+    """
+    pass
