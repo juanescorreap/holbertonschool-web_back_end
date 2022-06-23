39bcd0dd38dda559c6fb3671cd6deb5d8adea2de
juanescorreap <juanestebancope@outlook.com>
2022-06-23 09:21:58 -0500

    return session_id instead of user_id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -23,7 +23,7 @@ class SessionAuth(Auth):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
-        return user_id
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
