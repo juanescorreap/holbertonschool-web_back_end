25f2abb4a36d8c5e85cb382e9d525d19729008ef
juanescorreap <juanestebancope@outlook.com>
2022-06-23 09:11:10 -0500

    changes to calidation

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -58,7 +58,7 @@ def before_request() -> str:
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None\
+    if not auth.authorization_header(request)\
             and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
