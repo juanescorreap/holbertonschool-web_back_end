fde9aa5957be4409554bafd366df7f5dda5cd8ef
juanescorreap <juanestebancope@outlook.com>
2022-06-29 17:30:38 -0500

    implement register_user, task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 Method that takes in a password string
 arguments and returns bytes
 """
+from db import DB
 import bcrypt
+from user import Base, User
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +14,21 @@ def _hash_password(password: str) -> bytes:
     arguments and returns bytes
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Method to register a new user in the database
+        """
+        if self._db.find_user_by(email=email):
+            raise ValueError('User {email} already exists')
+        hashed_password = _hash_password(password)
+        registered_user = self._db.add_user(email, hashed_password)
+        return registered_user
