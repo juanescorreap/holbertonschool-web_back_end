51f35ef4066a9ffebd7d36cda66c82672265da15
juanescorreap <juanestebancope@outlook.com>
2022-06-29 17:44:34 -0500

    flask route to return welcome message

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app that has a single GET route
+"""
+from flask import Flask, jsonify, abort, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def retrurn_message():
+    """
+    Method that returns a JSON welcome message
+    """
+    jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
