a4cb8b305ce2b4d1cea400dbb90ac9a2ebc3712d
juanescorreap <juanestebancope@outlook.com>
2022-06-29 13:41:55 -0500

    testing to see what is missing

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
-"""DB module
+""" Database module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
+from typing import TypeVar
 
-from user import Base, User
+from user import Base
+from user import User
 
 
 class DB:
-    """DB class
+    """ Database class
+        Creates engine, session, adds user object to DB
+        Methods:
+            add_user - save the user object to the database
+            find_user_by - returns the first row found in the users table
     """
-
     def __init__(self):
-        """Initialize a new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """ constructor"""
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
-        """Memoized session object
-        """
+        """create session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Method that saves an user to the database
-        """
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ Add a user instance to the session DB """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
