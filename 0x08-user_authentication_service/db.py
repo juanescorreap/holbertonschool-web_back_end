1c9913817a632b99543e834841e0abc3fbae1c05
juanescorreap <juanestebancope@outlook.com>
2022-06-29 13:29:12 -0500

    delete second underscore to private properties

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -35,6 +35,6 @@ class DB:
         Method that saves an user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
-        self.__session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
