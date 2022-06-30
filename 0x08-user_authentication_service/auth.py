cc87179c956ca68a4cae88fee51c709c5992a6cb
juanescorreap <juanestebancope@outlook.com>
2022-06-30 18:42:05 -0500

    new methods and update documentation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Method that takes in a password string
-arguments and returns bytes
+Methods to handle the apps
+authentication and login related functions
 """
 from cmath import e
 from xmlrpc.client import Boolean
@@ -73,3 +73,49 @@ class Auth:
             session_id = str(uuid.uuid4())
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+        Method to retrieve a user
+        based on it's id
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Method that updates the session to None
+        in order to destroy it
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Mehtod to that generates an id and
+        resests the user's token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Method to update a user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
