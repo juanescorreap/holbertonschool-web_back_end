be9b4d71ca011551eed6b29011690ce3363a76ec
juanescorreap <juanestebancope@outlook.com>
2022-06-30 15:48:34 -0500

    let see

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -46,9 +46,9 @@ def login():
     if AUTH.valid_login(email, password) is False:
         abort(401)
     session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie('session_id', session_id)
-    return response
+    json_message = jsonify({'email': email, 'message': 'logged in'})
+    json_message.set_cookie('session_id', session_id)
+    return json_message
 
 
 if __name__ == "__main__":
