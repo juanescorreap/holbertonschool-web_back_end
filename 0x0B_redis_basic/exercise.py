1807b1b465ba413c29e7b946ca03c160ccf19929
juanescorreap <juanestebancope@outlook.com>
2022-07-19 12:02:08 -0500

    Constructor and store methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,26 @@
 Class that uses redis as a simple chache
 """
 import redis
+from typing import Union
+from uuid import uuid4
+
 
 class Cache():
-    def __init__():
-        _redis = redis.Redis()
+    """"
+    Class that uses redis as a simple chache
+    """
+    def __init__(self):
+        """
+        Constructor method
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Method that stores data in redis
+        using a randomly generated key
+        """
+        key = uuid4()
+        self._redis.set(key, data)
+        return key
