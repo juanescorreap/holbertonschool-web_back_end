d0039bb65d14595578fea4d22dcae58589640510
juanescorreap <juanestebancope@outlook.com>
2022-07-19 15:14:23 -0500

    exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Class that uses redis as a simple chache
 """
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -11,6 +11,7 @@ class Cache():
     """"
     Class that uses redis as a simple chache
     """
+
     def __init__(self):
         """
         Constructor method
@@ -26,3 +27,34 @@ class Cache():
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] =
+            None) -> Union[str, bytes, int, float]:
+        """
+        Method to get data from redis and
+        convert it back to the desired format
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """"
+        Method to get data from redis
+        and parametrize Cache.get to str
+        """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """"
+        Method to get data from redis
+        and parametrize Cache.get to str
+        """
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
