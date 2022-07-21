24a9e8666c615617e10f93997a710b1b69500d23
juanescorreap <juanestebancope@outlook.com>
2022-07-21 15:02:56 -0500

    idk

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,6 @@
 -- as their main style, ranked by their longevity
 SELECT DISTINCT band_name,
     IFNULL(split, YEAR(CURDATE())) - formed as lifespan
-FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
