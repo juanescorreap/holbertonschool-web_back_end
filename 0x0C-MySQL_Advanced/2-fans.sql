2717266f9dde1af95bc35f2e681d4fa739f65102
juanescorreap <juanestebancope@outlook.com>
2022-07-21 14:32:14 -0500

    delete space between the SUM key word and the parenthesis

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- SQL script that ranks country origins of bands
 -- ordered by the number of (non-unique) fans
 SELECT DISTINCT origin,
-    SUM (fans) as nb_fans
+    SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
