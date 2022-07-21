49870f999fd26e3f8e6615abd935fb6b58473421
juanescorreap <juanestebancope@outlook.com>
2022-07-21 17:21:20 -0500

    average score procedure

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,14 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student
-DELIMITER | CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id int) BEGIN
+DELIMITER |
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id int)
+BEGIN
 UPDATE users
 SET average_score = (
         SELECT AVG(score)
         FROM corrections AS c
         WHERE c.user_id = user_id
-    );
-UPDATE users
-SET average_score = avg_score
+    )
 WHERE id = user_id;
 END;
 |
