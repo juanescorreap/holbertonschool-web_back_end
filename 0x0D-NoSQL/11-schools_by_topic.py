37a6298fee80758f1a06db58e322dea7366b1005
juanescorreap <juanestebancope@outlook.com>
2022-07-26 14:58:23 -0500

    task 11

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Python function that returns the list
+of school having a specific topic
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Python function that returns the list
+    of school having a specific topic
+    """
+    return mongo_collection.find({"topic": topic})
