bc343066a846dffb39d8fa656bc7a4bdb2a5d9e2
juanescorreap <juanestebancope@outlook.com>
2022-07-26 14:32:16 -0500

    task 9

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -11,4 +11,4 @@ def insert_school(mongo_collection, **kwargs):
     Python function that inserts a new
     document in a collection based on kwargs
     """
-    return mongo_collection.insert(kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
