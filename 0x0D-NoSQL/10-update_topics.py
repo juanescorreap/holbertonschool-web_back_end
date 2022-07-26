d7e44718e80a88db6b83d199cfc605e72ba17db5
juanescorreap <juanestebancope@outlook.com>
2022-07-26 14:47:49 -0500

    task 10

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Python function that changes all topics
+of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Python function that changes all topics
+    of a school document based on the name
+    """
+    mongo_collection.update({name: name}, {'$set': {name: topics}})
