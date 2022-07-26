7d71c3c851efd62cac070c5c05960c7bba45d5ce
juanescorreap <juanestebancope@outlook.com>
2022-07-26 15:17:40 -0500

    task 12

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Python script that provides some stats
+about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+
+
+def method_requester(method_dict: dict) -> int:
+    """
+    Makes a request to the database
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(method_dict)
+
+
+def printer():
+    """
+    Prints stats about Nginx
+    logs stored in MongoDB
+    """
+    print(f"{method_requester({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {method_requester({'method': 'GET'})}")
+    print(f"\tmethod POST: {method_requester({'method': 'POST'})}")
+    print(f"\tmethod PUT: {method_requester({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {method_requester({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {method_requester({'method': 'DELETE'})}")
+    print(f"{method_requester({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    printer()
