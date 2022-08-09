61f96be0a5e2e97bd875fc65bfdb25ff26c6244e
juanescorreap <juanestebancope@outlook.com>
2022-08-09 18:02:36 -0500

    add validation and use startsWith method

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,13 @@
 export default function cleanSet(set, startString) {
-  const { length } = startString;
   const newString = '';
+  if (!startString || startString.length) {
+    return newString;
+  }
   for (const item of set) {
-    if (item === startString.substr(0, length)) {
-      newString.append(item, '-');
+    if (item === item.startsWith(startString)) {
+      newString.append(item.slice(startString.length));
+      newString.append('-');
     }
   }
-  return newString;
+  return newString.slice(0, -1);
 }
