e6e98f04d8955ee9360cd57c71b4c29607a4f860
juanescorreap <juanestebancope@outlook.com>
2022-08-09 18:39:43 -0500

    task 10, changed the way of accessing keys and values

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,9 @@ export default function updateUniqueItems(myMap) {
   if (!(myMap instanceof Map)) {
     throw Error('Cannot process');
   }
-  for (const item of myMap.values()) {
-    if (item === 1) {
-      myMap.set(item, 100);
+  for (const item of myMap) {
+    if (item[1] === 1) {
+      myMap.set(item[0], 100);
     }
   }
   return myMap;
