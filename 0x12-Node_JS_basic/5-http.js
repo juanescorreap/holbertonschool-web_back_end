5411e68318e8d0ec75e1356e41c2c3ed76090826
juanescorreap <juanestebancope@outlook.com>
2022-08-16 19:22:04 -0500

    task 5

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,20 +1,31 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+const port = 1245;
+
+// create a server object
+const app = http.createServer((req, res) => {
+  const { method, url } = req;
+  if (method === 'GET' && url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello Holberton School!'); // write a response to the client
+    res.end();
   }
-  res.end();
+  if (method === 'GET' && url === '/students') {
+    countStudents(String(process.argv.slice(2)))
+      .then((arrayOfClasses) => {
+        res.write('This is the list of our students\n'); // write a response to the client
+        res.write(`Number of students: ${arrayOfClasses.count}\n`);
+        for (const cls in arrayOfClasses) {
+          if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
+        }
+        res.end(); // end the response
+      })
+      .catch((err) => { throw err; });
+  }
 });
-app.listen(1245);
+
+app.listen(port);
+
 module.exports = app;
